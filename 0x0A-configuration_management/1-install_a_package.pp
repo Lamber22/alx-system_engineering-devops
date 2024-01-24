2f472d056f9edc2f46e97d3e4041ff63d543d4c9
Lamber22 <Lamberjoe5@gmail.com>
2024-01-23 20:45:52 -0800

    Install a package (update)

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
-# Install python3-pip package
-
-package { 'python3-pip':
-  ensure => 'installed',
-}
+#!/usr/bin/pup
 
 # Install flask using pip3
-
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3'

494354388a5be8c5be8b1b10c0e0bea7ca6f4c9e
Lamber22 <Lamberjoe5@gmail.com>
2024-01-23 20:50:15 -0800

    Install a package (update)

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,18 @@
-#!/usr/bin/pup
+# install flask
+
+# Install python3-pip package
+package { 'python3-pip':
+  ensure => 'installed',
+}
 
 # Install flask using pip3
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+# Install Werkzeug using pip3
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
